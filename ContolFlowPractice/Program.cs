539f9a53bee148a1d371cab0a04cb5151ab08e49
17193 <asialevesquegault@gmail.com>
2021-01-13 12:37:04 -0700

    Add project files.

---
+++ ContolFlowPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ContolFlowPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //CW enter a valid number:
+            Console.WriteLine("Please enter a valid number between 1-10:");
+            //USER INPUT Console.readline()
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //IF VALID(1<=x <= 10): CW(ToString(Readline stored as var) + "is valid")
+            int min = 1;
+            int max = 10;
+            if (min <= userInput == true && userInput<=max)
+            {
+                Console.WriteLine( userInput + " is Valid");
+            }
+            //ELSE CW("Please enter a valid number" )
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+        }
+    }
+}
